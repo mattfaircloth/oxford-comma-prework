6bc3c5ff5ea7fb95d61b18b9a0675fc74c0db71b
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-24 19:56:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  final_array = []
+  array.each_with_index do |fruit, i|
+    final_array.push(fruit)
+  end
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length > 2
+    final_array[array.length - 1] = "and ".concat(final_array[array.length-1]);
+    final_array = final_array.join(", ");
+ end
 end
